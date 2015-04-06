b9cde0656aa9d0520c3b162140b258ece6407adc
Sam Mitchell Finnigan <samuel.finnigan@ncl.ac.uk>
2015-04-06 16:36:20 +0000

    Runners execute correctly from any directory

--- runsub.sh
+++ runsub.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 while true;
 do
-    echo "Running STOMP publish script"
-	./publish.py -c config.ini $@
+    echo "Running STOMP to MongoDB bridge"
+	./subscribe.py -c config.ini $@
 	sleep 5
 done
